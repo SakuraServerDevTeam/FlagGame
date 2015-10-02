7fa5d1ad07d37fd8ef5fe3b8dec55944d3e1d8a2
toyblocks <spica.toyblocks@gmail.com>
2015-10-03 03:53:29 +0900

    Close #7

--- src/main/java/syam/flaggame/util/Cuboid.java
+++ src/main/java/syam/flaggame/util/Cuboid.java
@@ -9,87 +9,92 @@ import org.bukkit.block.Block;
 
 /**
  * 立体領域を表すクラス
- * 
+ *
  * @author syam
  */
 public class Cuboid {
+
+    private final Location min, max;
+
     /**
      * コンストラクタ
-     * 
-     * @param point1
-     *            Pos1
-     * @param point2
-     *            Pos2
+     *
+     * @param point1 Pos1
+     * @param point2 Pos2
      */
     public Cuboid(Location point1, Location point2) {
-        // 対角点設定
-        this.pos1 = point1;
-        this.pos2 = point2;
-
+        if (!point1.getWorld().equals(point2.getWorld())) {
+            throw new IllegalArgumentException("Both of two points must be in the same world!");
+        }
         // 各頂点設定
-        this.xMin = Math.min(point1.getBlockX(), point2.getBlockX());
-        this.xMax = Math.max(point1.getBlockX(), point2.getBlockX());
-        this.yMin = Math.min(point1.getBlockY(), point2.getBlockY());
-        this.yMax = Math.max(point1.getBlockY(), point2.getBlockY());
-        this.zMin = Math.min(point1.getBlockZ(), point2.getBlockZ());
-        this.zMax = Math.max(point1.getBlockZ(), point2.getBlockZ());
-
-        // ワールド設定
-        this.world = point1.getWorld();
+        int xMin = Math.min(point1.getBlockX(), point2.getBlockX());
+        int xMax = Math.max(point1.getBlockX(), point2.getBlockX());
+        int yMin = Math.min(point1.getBlockY(), point2.getBlockY());
+        int yMax = Math.max(point1.getBlockY(), point2.getBlockY());
+        int zMin = Math.min(point1.getBlockZ(), point2.getBlockZ());
+        int zMax = Math.max(point1.getBlockZ(), point2.getBlockZ());
+        this.min = new Location(point1.getWorld(), xMin, yMin, zMin);
+        this.max = new Location(point1.getWorld(), xMax, yMax, zMax);
     }
-
-    private final Location pos1, pos2;
-    private final int xMin, xMax, yMin, yMax, zMin, zMax;
-    private final World world;
 
     /**
      * 指定した座標が立体領域内かチェック
-     * 
-     * @param loc
-     *            チェックする座標
+     *
+     * @param loc チェックする座標
      * @return 領域内ならtrue 違えばfalse
      */
     public boolean isIn(Location loc) {
-        if (loc.getWorld() != this.world) return false;
-        if (loc.getBlockX() < xMin) return false;
-        if (loc.getBlockX() > xMax) return false;
-        if (loc.getBlockY() < yMin) return false;
-        if (loc.getBlockY() > yMax) return false;
-        if (loc.getBlockZ() < zMin) return false;
-        if (loc.getBlockZ() > zMax) return false;
-        return true;
+        if (loc.getWorld() != this.getWorld()) {
+            return false;
+        }
+        if (loc.getBlockX() < min.getBlockX()) {
+            return false;
+        }
+        if (loc.getBlockX() > max.getBlockX()) {
+            return false;
+        }
+        if (loc.getBlockY() < min.getBlockY()) {
+            return false;
+        }
+        if (loc.getBlockY() > max.getBlockY()) {
+            return false;
+        }
+        if (loc.getBlockZ() < min.getBlockZ()) {
+            return false;
+        }
+        return loc.getBlockZ() <= max.getBlockX();
     }
 
     /**
      * X軸の幅を取得(int)
-     * 
+     *
      * @return X軸の幅
      */
     public int getXWidth() {
-        return xMax - xMin;
+        return max.getBlockX() - min.getBlockX();
     }
 
     /**
      * Y軸の幅を取得(int)
-     * 
+     *
      * @return Y座標の幅
      */
     public int getZWidth() {
-        return zMax - zMin;
+        return max.getBlockZ() - min.getBlockZ();
     }
 
     /**
      * Z幅を取得(int)
-     * 
-     * @return Z座標の幅
+     *
+     * @return Y座標の幅
      */
     public int getHeight() {
-        return yMax - yMin;
+        return max.getBlockY() - min.getBlockY();
     }
 
     /**
      * 指定領域全体のブロック数を取得
-     * 
+     *
      * @return X幅*Y幅*高さ (int)
      */
     public int getArea() {
@@ -98,17 +103,17 @@ public class Cuboid {
 
     /**
      * 指定領域内のブロックをリストに入れて返す
-     * 
+     *
      * @return 領域内のブロックリスト
      */
     public List<Block> getBlocks() {
-        List<Block> blocks = new ArrayList<Block>();
+        List<Block> blocks = new ArrayList<>();
 
         // 全軸を最小値から最大値まで回す
-        for (int x = xMin; x <= xMax; x++) {
-            for (int y = yMin; y <= yMax; y++) {
-                for (int z = zMin; y <= zMax; z++) {
-                    blocks.add(world.getBlockAt(x, y, z)); // リストにブロックを追加
+        for (int x = min.getBlockX(); x <= max.getBlockX(); x++) {
+            for (int y = min.getBlockY(); y <= max.getBlockY(); y++) {
+                for (int z = min.getBlockZ(); y <= max.getBlockZ(); z++) {
+                    blocks.add(this.getWorld().getBlockAt(x, y, z)); // リストにブロックを追加
                 }
             }
         }
@@ -119,28 +124,28 @@ public class Cuboid {
     /* getter / setter */
     /**
      * ワールドを返す
-     * 
+     *
      * @return
      */
     public World getWorld() {
-        return world;
+        return min.getWorld();
     }
 
     /**
      * 対角点1を返す
-     * 
+     *
      * @return pos1
      */
     public Location getPos1() {
-        return pos1;
+        return min;
     }
 
     /**
      * 対角点2を返す
-     * 
+     *
      * @return pos2
      */
     public Location getPos2() {
-        return pos2;
+        return max;
     }
 }
