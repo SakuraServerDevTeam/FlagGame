714212f7c4c83a7b74a8f5724d967f0b883f7cf9
toyblocks <a@llv.jp>
2017-03-24 02:00:40 +0900

    Fix NPE

--- src/main/java/jp/llv/flaggame/util/ValueSortedMap.java
+++ src/main/java/jp/llv/flaggame/util/ValueSortedMap.java
@@ -16,36 +16,45 @@
  */
 package jp.llv.flaggame.util;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Map.Entry;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.NavigableMap;
-import java.util.NavigableSet;
+import java.util.Objects;
 import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author toyblocks
  */
-public class ValueSortedMap<K, V> implements SortedMap<K, V> {
+public class ValueSortedMap<K, V> implements Map<K, V> {
     
-    private TreeMap<K, V> map;
+    private final Comparator<Entry<K, V>> comparator;
+    private final TreeSet<Entry<K, V>> map;
     
-    public ValueSortedMap(Comparator<? super V> valueComparator) {
-        Comparator<K> keyComparator = (k1, k2) -> {
-            V v1 = map.get(k1), v2 = map.get(k2);
-            return valueComparator.compare(v1, v2);
-        };
-        map = new TreeMap<>(keyComparator);
+    public ValueSortedMap(Comparator<? super V> comparator) {
+        this.comparator = (e1, e2) -> comparator.compare(e1.getValue(), e2.getValue());
+        this.map = new TreeSet<>(this.comparator);
     }
-    
-    public ValueSortedMap(Map<? extends K, ? extends V> source, Comparator<? super V> valueComparator) {
-        this(valueComparator);
-        map.putAll(source);
+
+    @Override
+    public Set<K> keySet() {
+        return Collections.unmodifiableSet(map.stream().map(Entry::getKey).collect(Collectors.toSet()));
+    }
+
+    @Override
+    public Collection<V> values() {
+        return Collections.unmodifiableCollection(map.stream().map(Entry::getValue).collect(Collectors.toList()));
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return map;
     }
 
     @Override
@@ -54,180 +63,60 @@ public class ValueSortedMap<K, V> implements SortedMap<K, V> {
     }
 
     @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
     public boolean containsKey(Object key) {
-        return map.containsKey(key);
+        return map.stream().map(Entry::getKey).anyMatch(k -> Objects.equals(key, k));
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return map.containsValue(value);
+        return map.stream().map(Entry::getValue).anyMatch(v -> Objects.equals(value, v));
     }
 
     @Override
     public V get(Object key) {
-        return map.get(key);
-    }
-
-    @Override
-    public Comparator<? super K> comparator() {
-        return map.comparator();
-    }
-
-    @Override
-    public K firstKey() {
-        return map.firstKey();
-    }
-
-    @Override
-    public K lastKey() {
-        return map.lastKey();
-    }
-
-    @Override
-    public void putAll(Map<? extends K, ? extends V> map) {
-        this.map.putAll(map);
+        return map.stream().filter(e -> e.getKey().equals(key)).findFirst().map(Entry::getValue).orElse(null);
     }
 
     @Override
     public V put(K key, V value) {
-        return map.put(key, value);
+        Objects.requireNonNull(key);
+        for (Entry<K, V> entry : map) {
+            if (key.equals(entry.getKey())) {
+                V prev = entry.getValue();
+                entry.setValue(value);
+                return prev;
+            }
+        }
+        map.add(new SimpleEntry<>(key, value));
+        return null;
     }
 
     @Override
     public V remove(Object key) {
-        return map.remove(key);
+        Iterator<Entry<K, V>> it = map.iterator();
+        while (it.hasNext()) {
+            Entry<K, V> entry = it.next();
+            if (entry.getKey().equals(key)) {
+                it.remove();
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        m.entrySet().stream().forEach(e -> put(e.getKey(), e.getValue()));
     }
 
     @Override
     public void clear() {
         map.clear();
-    }
-
-    public Entry<K, V> firstEntry() {
-        return map.firstEntry();
-    }
-
-    public Entry<K, V> lastEntry() {
-        return map.lastEntry();
-    }
-
-    public Entry<K, V> pollFirstEntry() {
-        return map.pollFirstEntry();
-    }
-
-    public Entry<K, V> pollLastEntry() {
-        return map.pollLastEntry();
-    }
-
-    public Entry<K, V> lowerEntry(K key) {
-        return map.lowerEntry(key);
-    }
-
-    public K lowerKey(K key) {
-        return map.lowerKey(key);
-    }
-
-    public Entry<K, V> floorEntry(K key) {
-        return map.floorEntry(key);
-    }
-
-    public K floorKey(K key) {
-        return map.floorKey(key);
-    }
-
-    public Entry<K, V> ceilingEntry(K key) {
-        return map.ceilingEntry(key);
-    }
-
-    public K ceilingKey(K key) {
-        return map.ceilingKey(key);
-    }
-
-    public Entry<K, V> higherEntry(K key) {
-        return map.higherEntry(key);
-    }
-
-    public K higherKey(K key) {
-        return map.higherKey(key);
-    }
-
-    @Override
-    public Set<K> keySet() {
-        return map.keySet();
-    }
-
-    public NavigableSet<K> navigableKeySet() {
-        return map.navigableKeySet();
-    }
-
-    public NavigableSet<K> descendingKeySet() {
-        return map.descendingKeySet();
-    }
-
-    @Override
-    public Collection<V> values() {
-        return map.values();
-    }
-
-    @Override
-    public Set<Entry<K, V>> entrySet() {
-        return map.entrySet();
-    }
-
-    public NavigableMap<K, V> descendingMap() {
-        return map.descendingMap();
-    }
-
-    public NavigableMap<K, V> subMap(K fromKey, boolean fromInclusive, K toKey, boolean toInclusive) {
-        return map.subMap(fromKey, fromInclusive, toKey, toInclusive);
-    }
-
-    public NavigableMap<K, V> headMap(K toKey, boolean inclusive) {
-        return map.headMap(toKey, inclusive);
-    }
-
-    public NavigableMap<K, V> tailMap(K fromKey, boolean inclusive) {
-        return map.tailMap(fromKey, inclusive);
-    }
-
-    @Override
-    public SortedMap<K, V> subMap(K fromKey, K toKey) {
-        return map.subMap(fromKey, toKey);
-    }
-
-    @Override
-    public SortedMap<K, V> headMap(K toKey) {
-        return map.headMap(toKey);
-    }
-
-    @Override
-    public SortedMap<K, V> tailMap(K fromKey) {
-        return map.tailMap(fromKey);
-    }
-
-    @Override
-    public boolean replace(K key, V oldValue, V newValue) {
-        return map.replace(key, oldValue, newValue);
-    }
-
-    @Override
-    public V replace(K key, V value) {
-        return map.replace(key, value);
-    }
-
-    @Override
-    public void forEach(BiConsumer<? super K, ? super V> action) {
-        map.forEach(action);
-    }
-
-    @Override
-    public void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
-        map.replaceAll(function);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return map.isEmpty();
     }
     
 }
