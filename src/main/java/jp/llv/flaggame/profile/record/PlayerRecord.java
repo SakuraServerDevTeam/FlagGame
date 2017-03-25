a3d29893342fa42979e58137a982afd31a253b1a
toyblocks <a@llv.jp>
2017-03-25 17:01:36 +0900

    Unify author in license headers

--- src/main/java/jp/llv/flaggame/profile/record/PlayerRecord.java
+++ src/main/java/jp/llv/flaggame/profile/record/PlayerRecord.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2017 Toyblocks, SakuraServerDev
+ * Copyright (C) 2017 SakuraServerDev
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
